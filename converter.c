0b51597159a31d9475ff9bf8a65969f9cbec5850
Mercy <mercybhakta@yahoo.com>
2018-12-13 08:43:28 +0000

    Adding the assignment to github

---
+++ converter.c
@@ -0,0 +1,29 @@
+/*
+ * Trivial implementation of converter function.
+ * You may override this file or create your own.
+ */
+#include "converter.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+void convert_all(unsigned nlines, char *lines[], quote_t nums[])
+{
+
+
+  for (unsigned i = 0; i < nlines; i++)
+  {
+    // An inline atoi() function
+    //with help from: https://www.geeksforgeeks.org/write-your-own-atoi/
+    char * str = lines[i];
+    int num = 0;
+    // Iterate through all characters of input string and
+    // update result
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        num = num*10 + str[i] - '0';
+    }
+    // return result.
+    nums[i] = num;
+  }
+}
